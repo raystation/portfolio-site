41ba1022d574eca3849bdcc0905ef2c226437d84
raystation <rayuen@gmail.com>
2014-03-20 12:16:14 -0700

    initial commit

---
+++ inc/footer.php
@@ -0,0 +1,78 @@
+	<!-- Footer
+	==================================================  -->
+
+	<footer class="band">
+
+		<div class="container">
+			
+			<div class="four columns">
+
+				<ul>
+					<li><a href="index.php">home</a></li>
+					<li><a href="portfolio.php">portfolio</a></li>
+					<li><a href="about.php">about</a></li>
+					<li><a href="#">news</a></li>
+				</ul>
+
+			</div>
+			<div class="four columns">
+
+				<p>
+					Riker's background is first explored in the second-season episode The Icarus Factor.
+				</p>
+
+			</div>
+			<div class="four columns">
+
+				<p>
+					Riker's background is first explored in the second-season episode The Icarus Factor.
+				</p>
+
+			</div>
+			<div class="four columns">
+
+				<p>
+					Riker's background is first explored in the second-season episode The Icarus Factor.
+				</p>
+
+			</div>
+
+			<div class="sixteen columns">
+				<hr class="footer">
+				<p> &copy <?php echo date('Y');?> 
+					Raymond Yuen • made with Skeleton • 
+				</p>
+
+			</div>
+
+		</div> <!-- END CONTAINER -->
+
+	</footer>
+	
+	<!-- Bxslider JS
+	================================================== -->
+	<script src="js/jquery/1.8.2/jquery.min.js"></script>
+	<script src="js/jquery.bxslider.js"></script>
+	
+	<script>
+		$(document).ready(function(){
+		  $('.bxslider').bxSlider();
+		});
+
+		$(document).ready(function(){
+		  $('.bxsliderIndex').bxSlider({
+		  	easing: 'ease-in-out',
+		  	autoHover: true,
+		  });
+		});
+	</script>
+
+	<!-- Lightbox 
+	==================================================  -->
+	<script src="js/lightbox-2.6.min.js"></script>
+	
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
---
+++ portfolio.php
@@ -0,0 +1,231 @@
+<?php 
+	$pageTitle = "Ray Yuen | Portfolio";
+	include('inc/header.php'); ?>
+
+	<!-- Row 1
+	================================================== -->
+
+	<div class="container portfolio">
+		
+		<div class="four columns">
+			<a href="img/animore/01.jpg" data-lightbox="animore" title=""><img src="img/animore/thumb.jpg" class="scale-with-grid"></a>
+			<p>Animore</p>
+		</div>
+
+		<div class="four columns">
+			<a href="img/edgeware/00.jpg" data-lightbox="edgeware" title=""><img src="img/edgeware/00.jpg" class="scale-with-grid"></a>
+			<p>Edgeware</p>
+		</div>
+
+		<div class="four columns">
+			<a href="img/idibon/derby2-01.jpg" data-lightbox="idibon"><img src="img/idibon/title.jpg" class="scale-with-grid"></a>
+			<p>Idibon</p>
+		</div>
+
+		<div class="four columns">
+			<a href="img/maladobaldwin/01.jpg" data-lightbox="malado"><img src="img/maladobaldwin/title.jpg" class="scale-with-grid"></a>
+			<p> Malado Baldwin</p>
+		</div>
+
+	</div>
+	
+
+	<!-- Row 2
+	================================================== -->
+	
+	<div class="container portfolio">
+		
+		<div class="four columns">
+			<a href="img/eggbaby_b.jpg" data-lightbox="eggbaby"><img src="img/eggbaby1.jpg" class="scale-with-grid">
+			<p>Egg Baby</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="img/indiginauts_b.jpg" data-lightbox="indigi"><img src="img/indiginauts_b.jpg" class="scale-with-grid">
+			<p>Indiginauts</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="img/mky/cover.jpg" class="visible" data-lightbox="mky"><img src="img/mky/thumb.jpg" class="scale-with-grid">
+			<p>Monkey Mo, Monkey&nbsp;Go</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="img/museyon/title_b.jpg" data-lightbox="museyon"><img src="img/museyon/title.jpg" class="scale-with-grid">
+			<p>Museyon</p></a>
+		</div>
+
+	</div>	
+	
+	
+	<!-- Row 3
+	================================================== -->
+		
+	<div class="container portfolio">
+		
+		<div class="four columns">
+			<a href="img/museyon/c-ny/title.jpg" data-lightbox="pokemon"><img src="img/museyon/c-ny/title.jpg" class="scale-with-grid">
+			<p>Pokémon</p></a>
+			
+		</div>
+
+		<div class="four columns">
+			<a href="img/museyon/rome/cover.jpg" data-lightbox="rome"><img src="img/museyon/rome/thumb.jpg" class="scale-with-grid">
+			<p>Chronicles of Old&nbsp;Rome</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="img/pearson/attention.jpg" data-lightbox="pearson"><img src="img/pearson/title.jpg" class="scale-with-grid">
+			<p>100 Principles of Game&nbsp;Design</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="img/storymaps/01.jpg" data-lightbox="storymaps"><img src="img/storymaps/title.jpg" class="scale-with-grid">
+			<p>Storymaps</p></a>
+		</div>
+
+	</div>
+	
+	
+	<!-- Row 4
+	================================================== -->
+	
+	<div class="container portfolio">
+			
+		<div class="four columns">
+			<a href="img/" data-lightbox="poke"><img src="img/pkmn/title.jpg" class="scale-with-grid">
+			<p>Pokémon</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="img/scavhunt/01.jpg" data-lightbox="hunt"><img src="img/scavhunt/title.jpg" class="scale-with-grid">
+			<p>Scavenger Hunt</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="img/colorRunner/05.jpg" data-lightbox="runner"><img src="img/colorRunner/title.jpg" class="scale-with-grid">
+			<p>Color Runner</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="img/tiger/tigertiger_b.jpg" data-lightbox="tiger"><img src="img/tiger/title.jpg" class="scale-with-grid">
+			<p>Tiger, Tiger Tail on&nbsp;Fire</p></a>
+		</div>
+
+	</div>
+	
+	
+	<!-- Row 5
+	================================================== -->
+	
+	<div class="container portfolio">
+			
+		<div class="four columns">
+			<a href="img/walko/walko1.jpg" data-lightbox="walko"><img src="img/walko/walko_thumb.jpg" class="scale-with-grid">
+			<p>Sarah Walko</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="img/superbarracuda/Palin_halloween.jpg" data-lightbox="palin"><img src="img/superbarracuda/palin_thumb.jpg" class="scale-with-grid">
+			<p>Sarah Palin</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="gif-fun.php"><img src="img/dailygifs/thumbnail.gif" class="scale-with-grid">
+			<p>GIF fun!</p></a>
+		</div>
+
+		<div class="four columns">
+			<a href="img/chrichard/Chrichard-invite1.png" data-lightbox="chrichard"><img src="img/chrichard/thumbnail.jpg" class="scale-with-grid">
+			<p>Chrichard invite</p></a>
+		</div>
+
+	</div>
+	
+	
+	<!-- Hidden images
+	================================================== -->
+	
+	<div style="display:none">
+		<a href="img/animore/02.jpg" data-lightbox="animore"></a>
+		<a href="img/animore/03.jpg" data-lightbox="animore"></a>
+		<a href="img/animore/04.jpg" data-lightbox="animore"></a>
+		<a href="img/animore/05.jpg" data-lightbox="animore"></a>
+		<a href="img/animore/06.jpg" data-lightbox="animore"></a>
+		<a href="img/animore/07.jpg" data-lightbox="animore"></a>
+		<a href="img/animore/08.jpg" data-lightbox="animore"></a>
+		<a href="img/animore/09.jpg" data-lightbox="animore"></a>
+		<a href="img/animore/10.jpg" data-lightbox="animore"></a>
+
+		<a href="img/edgeware/00.jpg" data-lightbox="edgeware"></a>
+		<a href="img/edgeware/02.jpg" data-lightbox="edgeware"></a>
+		<a href="img/edgeware/03.jpg" data-lightbox="edgeware"></a>
+		<a href="img/edgeware/04.jpg" data-lightbox="edgeware"></a>
+		<a href="img/edgeware/05.jpg" data-lightbox="edgeware"></a>
+
+		<a href="img/idibon/bball_observed_hq.jpg" data-lightbox="idibon"></a>
+		<a href="img/idibon/graphs_Percentage_hq.jpg" data-lightbox="idibon"></a>
+		<a href="img/idibon/graphs_Song_Titles_hq.jpg" data-lightbox="idibon"></a>
+		<a href="img/idibon/powerhouse-language_v.png" data-lightbox="idibon"></a>
+		<a href="img/idibon/judge.jpg" data-lightbox="idibon"></a>
+	
+		<a href="img/maladobaldwin/01.jpg" data-lightbox="malado"></a>
+		<a href="img/maladobaldwin/02.jpg" data-lightbox="malado"></a>
+		<a href="img/maladobaldwin/03.jpg" data-lightbox="malado"></a>
+		<a href="img/maladobaldwin/04.jpg" data-lightbox="malado"></a>
+		<a href="img/maladobaldwin/05.jpg" data-lightbox="malado"></a>
+		<a href="img/maladobaldwin/06.jpg" data-lightbox="malado"></a>
+		<a href="img/maladobaldwin/07.jpg" data-lightbox="malado"></a>
+		<a href="img/maladobaldwin/08.jpg" data-lightbox="malado"></a>
+
+		<a href="img/mky/monkey_b.jpg" data-lightbox="mky"></a>		
+		<a href="img/mky/closeup.jpg" data-lightbox="mky"></a>		
+		<a href="img/mky/circus.jpg" data-lightbox="mky"></a>		
+		<a href="img/mky/animals.jpg" data-lightbox="mky"></a>		
+		<a href="img/mky/closeup.jpg" data-lightbox="mky"></a>		
+		
+		<a href="img/museyon/rome/1.jpg" data-lightbox="rome"></a>
+		<a href="img/museyon/rome/2.jpg" data-lightbox="rome"></a>
+		<a href="img/museyon/rome/3.jpg" data-lightbox="rome"></a>
+		<a href="img/museyon/rome/4.jpg" data-lightbox="rome"></a>
+		
+		<a href="img/pearson/commons.jpg" data-lightbox="pearson"></a>
+		<a href="img/pearson/minmax.jpg" data-lightbox="pearson"></a>
+		<a href="img/pearson/environment.jpg" data-lightbox="pearson"></a>
+		<a href="img/pearson/errors.jpg" data-lightbox="pearson"></a>
+		<a href="img/pearson/griefing.gif" data-lightbox="pearson"></a>
+		<a href="img/pearson/hicks.jpg" data-lightbox="pearson"></a>
+		<a href="img/pearson/rewards.jpg" data-lightbox="pearson"></a>
+		<a href="img/pearson/rewards_2.jpg" data-lightbox="pearson"></a>
+		<a href="img/pearson/sandbox_1.jpg" data-lightbox="pearson"></a>
+		<a href="img/pearson/sandbox_2.jpg" data-lightbox="pearson"></a>
+		<a href="img/pearson/wayfinding.jpg" data-lightbox="pearson"></a>
+		<a href="img/pearson/working_memory.jpg" data-lightbox="pearson"></a>	
+
+		<a href="img/storymaps/02.jpg" data-lightbox="storymaps"></a>
+		<a href="img/storymaps/03.jpg" data-lightbox="storymaps"></a>
+		<a href="img/storymaps/04.jpg" data-lightbox="storymaps"></a>
+		<a href="img/storymaps/05.jpg" data-lightbox="storymaps"></a>
+		<a href="img/storymaps/06.jpg" data-lightbox="storymaps"></a>
+		
+		<a href="img/scavhunt/02.jpg" data-lightbox="hunt"></a>	
+		
+		<a href="img/colorRunner/01.jpg" data-lightbox="runner"></a>
+		<a href="img/colorRunner/02.jpg" data-lightbox="runner"></a>
+		<a href="img/colorRunner/03.jpg" data-lightbox="runner"></a>
+		<a href="img/colorRunner/04.jpg" data-lightbox="runner"></a>
+			
+		<a href="img/tiger/tiger_cover.png" data-lightbox="tiger"></a>
+		<a href="img/tiger/tiger_02.png" data-lightbox="tiger"></a>
+		<a href="img/tiger/tiger_03.png" data-lightbox="tiger"></a>
+		<a href="img/tiger/tiger_04.jpg" data-lightbox="tiger"></a>
+		<a href="img/tiger/tiger_05.jpg" data-lightbox="tiger"></a>
+		
+		<a href="img/walko/walko2.jpg" data-lightbox="walko"></a>
+		<a href="img/walko/walko3.jpg" data-lightbox="walko"></a>
+
+		<a href="img/chrichard/Chrichard-invite2.png" data-lightbox="chrichard"></a>		
+		
+	</div>
+
+<?php include('inc/footer.php'); ?>
