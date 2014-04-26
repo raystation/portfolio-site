480a7fdf88ebd460f1878c44cdbde3e61b89ad86
raystation <rayuen@gmail.com>
2014-04-25 18:14:00 -0700

    edited htaccess to make clean urls

--- inc/header.php
+++ inc/header.php
@@ -51,14 +51,14 @@
 
 		<nav class="sixteen columns">
 			
-			<h2 class="title"><a href="index.php">Ray Yuen</a></h2>
+			<h2 class="title"><a href="index">Ray Yuen</a></h2>
 			
 			<ul class="add-bottom">
-				<li><a href="work.php">work</a></li>
+				<li><a href="work">work</a></li>
 				<?php //<li>•</li>
 				//<li><a href="projects.php">projects</a></li> ?>
 				<li>•</li>
-				<li><a href="about.php">about</a></li>
+				<li><a href="about">about</a></li>
 			</ul>				
 
 		</nav>
