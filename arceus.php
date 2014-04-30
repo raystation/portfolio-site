43a109313623fd3fb5c9faa6c154968befc31e9e
raystation <rayuen@gmail.com>
2014-04-30 09:03:50 -0700

    added in Arceus

---
+++ arceus.php
@@ -0,0 +1,6 @@
+<?php 
+	$title = "Arceus and the Jewel of Life";
+	$pageTitle = "Ray Yuen | " . $title;
+	include('inc/header.php');?>
+<?php include('inc/content.php'); ?>	
+<?php include('inc/footer.php'); ?>
