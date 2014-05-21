a5694050fcd7a7aaa259801f8ab36bbcb7779db2
Ray <rayuen@gmail.com>
2014-05-20 23:48:27 -0700

    changed username

--- inc/content.php
+++ inc/content.php
@@ -3,32 +3,33 @@
 		<div class="sixteen columns">
 			
 			<?php  
-				// echo "<div class='slick'>\n";
-				// 	foreach($img_info as $img) {
-				// 	    echo 
-				// 	    "\t\t\t\t<div><img class=\"scale-with-grid add-bottom\" src=\"".$path.$img["file"]."\" alt=\"".$img["alt"]."\"></div>\n";
-				// 	}
-				// echo "\t\t\t</div>";
-				
-
-				$path = ( strtolower ( $title ) );
-				$path = ( str_replace(" ", "-", $path ) );
-
-				$imgpath = "img/".$path."/";
-				$images = scandir( $imgpath );
-
 				echo "<div class='slick'>\n";
-				foreach ( $images as $image ) {
-					
-					if ( strpos( $image,'jpg' ) || strpos( $image,'png' ) ) { 
-						$alt_text = ( str_replace("-", " ", $image ) );
-						$alt_text = ( str_replace(".jpg", "", $alt_text ) );
-						$alt_text = ( str_replace(".png", "", $alt_text ) );
-						
-						echo "<div><img class= \"scale-with-grid add-bottom\" src=\"" .  $imgpath . $image . "\" alt=\"" .  $alt_text . "\"></div>\n";
+					foreach($img_info as $img) {
+					    echo 
+					    "\t\t\t\t<div><img class=\"scale-with-grid add-bottom\" src=\"".$path.$img["file"]."\" alt=\"".$img["alt"]."\"></div>\n";
 					}
-				}
-				echo "</div>\n";
+				echo "\t\t\t</div>";
+				
+				// CODE WILL READ DIRECTORY AND PUTS IMAGES TO THE SCREEN
+				
+				// $path = ( strtolower ( $title ) );
+				// $path = ( str_replace(" ", "-", $path ) );
+
+				// $imgpath = "img/".$path."/";
+				// $images = scandir( $imgpath );
+
+				// echo "<div class='slick'>\n";
+				// foreach ( $images as $image ) {
+					
+				// 	if ( strpos( $image,'jpg' ) || strpos( $image,'png' ) ) { 
+				// 		$alt_text = ( str_replace("-", " ", $image ) );
+				// 		$alt_text = ( str_replace(".jpg", "", $alt_text ) );
+				// 		$alt_text = ( str_replace(".png", "", $alt_text ) );
+						
+				// 		echo "<div><img class= \"scale-with-grid add-bottom\" src=\"" .  $imgpath . $image . "\" alt=\"" .  $alt_text . "\"></div>\n";
+				// 	}
+				// }
+				// echo "</div>\n";
 
 			;?>
 
