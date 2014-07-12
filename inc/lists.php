b12d0d98fa54033e99baf79d50c9e5d6d314c2a8
Ray <rayuen@gmail.com>
2014-07-12 00:26:11 -0700

    updated all lists to new style

--- inc/lists.php
+++ inc/lists.php
@@ -35,34 +35,37 @@ function random_list_items($array,$quantity=5) {
 	return $random_list;
 }
 
-function print_list( $array,$results=5,$randomize=false,$heading=6 ){
+function print_list( $array,$randomize=false,$results=5,$heading=5 ){
+	
+	// check to make sure the variables are not NULL
 	if ( !is_null( $array["name"] ) ) { $name=$array["name"]; }
 	if ( !is_null( $array["list"] ) ) { $array=$array["list"]; }
-	
-	if ( count($array) < $results ) { $random_numbers_array=array_rand($array, count($array) ); }
-	else { $random_numbers_array=array_rand($array,$results); }
 
-	if ( $randomize==true ) { $array=shuffle($array); }
-	echo "<h5>".$name."</h5>";
+	if ( $randomize==true ) { 
+		if ( count($array) < $results ) { 
+			$numbers_array=array_rand($array, count($array) ); 
+			shuffle($numbers_array); 
+		} else { 
+			$numbers_array=array_rand($array,$results); 
+			shuffle($numbers_array); 
+		}
+	} 
+
+	if ( $randomize==false ) {
+			if ( count($array) < $results ) { 
+				$numbers_array=range(0, count($array) ); 
+			} else { $numbers_array=range(0, $results); 
+		}
+	}
+	// var_dump($numbers_array);
+
+	echo "<h".$heading.">".$name."</h".$heading.">";
 	echo "<ul>";
 	
-	foreach ($random_numbers_array as $number) {
+	foreach ($numbers_array as $number) {
 		echo "<li>".$array[$number]."</li>";
 	}
 	echo "</ul>";
-}
-
-function list_games_play2() {
-	$container = array(
-		"name"=>"Games Playing",
-		"list"=>array(
-			"X-COM: Enemy Unknown",
-			"Disco Zoo",
-			"Nimblequest",
-			"Red Dead Redemption",
-		)
-	);
-	return $container;
 }
 
 function list_ringtones(){
@@ -92,118 +95,136 @@ function html_list_ringtones(){
 }
 
 function list_favorite_games() {
-	$list_title="Favorite Games";
-	$favorite_games = array(
-		"2048",
-		"Batman: Arkham Asylum",
-		"Batman: Arkham City",
-		"Bubble Bobble",
-		"Chrono Trigger",
-		"Crystal Castles",
-		"Dig Dug",
-		"Double Dragon",
-		"Fallout 3",
-		"Fallout: New Vegas",
-		"Final Fantasy 4",
-		"X-COM",
-		"Final Fantasy 6",
-		"Final Fantasy Tactics",
-		"Ghouls 'n' Ghosts",
-		"Golden Axe II",
-		"Golden Axe",
-		"Junk Jack",
-		"L.A. Noire",
-		"Marble Madness",
-		"Mario Kart 7",
-		"Mario Kart DS",
-		"Metal Gear Solid",
-		"Metal Gear Solid 3: Snake Eater",
-		"Michael Jackson's Moonwalker",
-		"Mickey Mouse and the Castle of Illusion",
-		"Mortal Kombat II",
-		"Mortal Kombat",
-		"Ms. Pac-man",
-		"NBA Jam",
-		"NBA Live 95",
-		"NBA Live 95",
-		"Okami",
-		"PaRappa the Rappa",
-		"Patapon",
-		"Phoenix Wright 2: Trials and Tribulations",
-		"Phoenix Wright: Ace Attorney",
-		"Plants vs. Zombies",
-		"Pocket Planes",
-		"Pokemon Diamond & Pearl",
-		"Trauma Center",
-		"Pokemon Y",
-		"Rampage",
-		"Red Dead Redemption",
-		"Resident Evil 4",
-		"Sim Hospital",
-		"SimCity 2000",
-		"Street Fighter II",
-		"Gunstar Heroes",
-		"Tapper!",
-		"Sonic the Hedgehog",
-		"Super Mario Brothers 3",
-		"Super Mario Kart",
-		"Super Punchout!",
-		"Tekken 3",
-		"Tenchu: Stealth Assassins",
-		"Tetris",
-		"The Last of Us",
-		"The Organ Trail",
-		"Tiny Thief",
-		"Triple Town",
-		"Toejam &amp; Earl",
-		"Transport Tycoon",
-		"Uncharted 2",
-		"Zombies Ate My Neighbors",
+	$list = array(
+		"name"=>"Favorite Games",
+		"list"=>array(
+			"2048",
+			"Batman: Arkham Asylum",
+			"Batman: Arkham City",
+			"Bubble Bobble",
+			"Chrono Trigger",
+			"Crystal Castles",
+			"Dig Dug",
+			"Double Dragon",
+			"Fallout 3",
+			"Fallout: New Vegas",
+			"Final Fantasy 4",
+			"X-COM",
+			"Final Fantasy 6",
+			"Final Fantasy Tactics",
+			"Ghouls 'n' Ghosts",
+			"Golden Axe II",
+			"Golden Axe",
+			"Junk Jack",
+			"L.A. Noire",
+			"Marble Madness",
+			"Mario Kart 7",
+			"Mario Kart DS",
+			"Metal Gear Solid",
+			"Metal Gear Solid 3: Snake Eater",
+			"Michael Jackson's Moonwalker",
+			"Mickey Mouse and the Castle of Illusion",
+			"Mortal Kombat II",
+			"Mortal Kombat",
+			"Ms. Pac-man",
+			"NBA Jam",
+			"NBA Live 95",
+			"Okami",
+			"PaRappa the Rappa",
+			"Patapon",
+			"Phoenix Wright 2: Trials and Tribulations",
+			"Phoenix Wright: Ace Attorney",
+			"Plants vs. Zombies",
+			"Pocket Planes",
+			"Pokemon Diamond & Pearl",
+			"Trauma Center",
+			"Pokemon Y",
+			"Rampage",
+			"Red Dead Redemption",
+			"Resident Evil 4",
+			"Sim Hospital",
+			"SimCity 2000",
+			"Street Fighter II",
+			"Gunstar Heroes",
+			"Tapper!",
+			"Sonic the Hedgehog",
+			"Super Mario Brothers 3",
+			"Super Mario Kart",
+			"Super Punchout!",
+			"Tekken 3",
+			"Tenchu: Stealth Assassins",
+			"Tetris",
+			"The Last of Us",
+			"The Organ Trail",
+			"Tiny Thief",
+			"Triple Town",
+			"Toejam &amp; Earl",
+			"Transport Tycoon",
+			"Uncharted 2",
+			"Zombies Ate My Neighbors",
+		)
 	);
-	return $favorite_games;
-}
-
-function list_tattoo_body() {
-	$list_title="Regretful Tattoos";
-	$container = array(
-		"Angel wings on my butt (from when I fell from heaven)",
-		"USB ports",
-		"Barcode for Ecto Cooler",
-		"My birthdate in Roman numerals",
-		"the one password I use for all my logins so I don't forget",
-		"Flaming unicorns",
-	);
-	return $container;
+	return $list;
 }
 
 function list_self_nicknames() {
-	$list_title="Nicknames I Made Up For Myself That Didn't&nbsp;Stick";
-	$container = array(
-		"Angel Eyes",
-		"Tiny Dancer",
-		"Dance Floor General",
-		"Lil'",
-		"Inspector",
-		"Asian Sensation",
-		"The Worm",
-		"The Hand"
+	$list = array(
+		"name"=>"Nicknames (Didn't&nbsp;Stick)",
+		"list"=>array(
+			"Angel Eyes",
+			"Tiny Dancer",
+			"Dance Floor General",
+			"Lil'",
+			"Inspector",
+			"Asian Sensation",
+			"The Worm",
+			"The Hand"
+		)
 	);
-	return $container;
+	return $list;	
+}
+
+function list_fav_artists() {
+	$list = array(
+		"name"=>"Favorite Art People",
+		"list"=>array(
+			"Pendleton Ward",
+			"Ji Lee",
+			"Hellen Jo",
+			"Jack Teagle",
+			"James Jean",
+			"Tom Gauld",
+			"Travis Millard",
+			"Peter Oumanski",
+			"Olly Moss",
+			"Mike Perry",
+			"eBoy",
+			"Geoff Mcfettridge",
+			"Mar&eacute; Odomo",
+			"Jordan Crane",
+			"Jim Houser",
+			"Josh Cochran",
+			"Bryan Lee O'Malley",
+			"Chuck Groenink",
+			"Jessica Hische",
+			"Genevi&eacute;ve Gauckler",
+			"Maya Hayuk",
+			"Neil Scwaab",
+			"Carson Ellis",
+		)
+	);
+	return $list;	
 }
 
 function list_games_playing() {
-	$list_title="Currently Playing";
 	$container = array(
-		"X-COM: Enemy Unknown",
-		"Disco Zoo",
-		"Nimblequest",
-		"Red Dead Redemption",
-		// "FTL",
-		// "Pok&eacute;mon Y",
-		// "The Last of Us",
-		// "F E Z",
-		// "Zookeeper: Battle!",
-		// "Skyrim",
+		"name"=>"Games Playing",
+		"list"=>array(
+			"X-COM: Enemy Unknown",
+			"Disco Zoo",
+			"Nimblequest",
+			"Red Dead Redemption",
+		)
 	);
 	return $container;
 }
@@ -219,115 +240,46 @@ function list_random_facts() {
 }
 
 function list_albums_listening() {
-	$container = array(
-		"Paracosm, <em>Washed&nbsp;Out</em>",
-		"St. Vincent, <em>St.&nbsp;Vincent</em>",
-		"Modern Vampires in the City, <em>Vampire&nbsp;Weekend</em> ",
-		"Ritual of the Savage, <em>Les&nbsp;Baxter</em>",
-		"Gunfighter Ballads and Trail Songs, <em>Marty&nbsp;Robbins</em>",
+	$list = array(
+			"name"=>"Albums Listening to",
+			"list"=>array(
+				"Paracosm, <em>Washed&nbsp;Out</em>",
+				"St. Vincent, <em>St.&nbsp;Vincent</em>",
+				"Modern Vampires in the City, <em>Vampire&nbsp;Weekend</em> ",
+				"Ritual of the Savage, <em>Les&nbsp;Baxter</em>",
+				"Gunfighter Ballads and Trail Songs, <em>Marty&nbsp;Robbins</em>",
+		)
 	);
-	return $container;
+	return $list;
 }
 
-function all_lists(){
-
-	$lists=array(
-		array( "title"=>"Nicknames", "items"=>array(
-				"Angel Eyes",
-				"Tiny Dancer",
-				"Dance Floor General",
-				"Lil",
-				"Inspector",
-				"Asian Sensation",
-				"The Worm",
-				"The Hand"
-			)),
-
-		array( "title"=>"Random Facts About Me", "items"=>array(
-				"I’ve illustrated two children’s books.",
-				"I drew caricatures at Six Flags Great Adventure in New Jersey for about four summers.",
-				"I can solve a Rubik’s Cube in under a minute.",
-				"I worked at a toy truck modeling company doing screen and pad printing.",
-			)),
-
-		array( "title"=>"Currently Playing", "items"=>array(
-				"FTL",
-				"The Last of Us",
-				"Red Dead Redemption",
-				// "F E Z",
-				"X-COM: Enemy Unknown",
-				"Pok&eacute;mon Y",
-				// "Zookeeper: Battle!",
-				// "Skyrim",
-				"Disco Zoo",
-				"Nimblequest",
-			)),		
-
-		array( "title"=>"Favorite Games", "items"=>array(
-				"Mario Kart 7",
-				"Super Mario Kart",
-				"Mario Kart DS",
-				"Phoenix Wright 2: Trials and Tribulations",
-				"Phoenix Wright: Ace Attorney",
-				"Ms. Pac-man",
-				"Golden Axe",
-				"Bubble Bobble",
-				"Dig Dug",
-				"Crystal Castles",
-				"Mickey Mouse and the Castle of Illusion",
-				"Michael Jackson's Moonwalker",
-				"Super Punchout!",
-				"Golden Axe II",
-				"The Last of Us",
-				"Red Dead Redemption",
-				"Tiny Thief",
-				"Tekken 3",
-				"NBA Live 95",
-				"NBA Live 95",
-				"Mortal Kombat II",
-				"Mortal Kombat",
-				"NBA Jam",
-				"Final Fantasy 4",
-				"Final Fantasy 6",
-				"Chrono Trigger",
-				"Fallout: New Vegas",
-				"Fallout 3",
-				"Batman: Arkham Asylum",
-				"Batman: Arkham City",
-				"L.A. Noire",
-				"PaRappa the Rappa",
-				"Patapon",
-				"Tetris",
-				"Okami",
-				"Double Dragon",
-				"Resident Evil 4",
-				"Sonic the Hedgehog",
-				"Super Mario Brothers 3",
-				"Zombies Ate My Neighbors",
-				"Toejam and Earl",
-				"Plants vs. Zombies",
-				"Final Fantasy Tactics",
-				"Pokemon Diamond & Pearl",
-				"Pokemon White",
-				"Pokemon Y",
-				"Uncharted 2",
-				"Tenchu: Stealth Assassins",
-				"Metal Gear Solid",
-				"Pocket Planes",
-				"Transport Tycoon",
-				"Sim Hospital",
-				"Junk Jack",
-				"Marble Madness",
-				"Rampage",
-				"Ghouls 'n' Ghosts",
-				"2048",
-				"The Organ Trail",
-				"SimCity 2000"
-			)),		
+function list_fav_tv() {
+	$list = array(
+			"name"=>"Favorite TV Shows",
+			"list"=>array(
+				"LOST",
+				"Venture Brothers",
+				"Bravest Warriors",
+				"Sherlock",
+				"Breaking Bad",
+				"Star Trek: TOS",
+				"Battlestar Galactica",
+				"Twilight Zone",
+				"Mad Men",
+				"SNL",
+				"Game of Thrones",
+				"BBC Planet Earth",
+				"Firefly",
+				"Arrested Development",
+				"Cowboy Bebop",
+				"Doctor Who",
+				"Chappelle's Show",
+				"Futurama",
+				"Mighty Boosh",
+				"Archer",
+		)
 	);
-
-	return $lists;
+	return $list;
 }
-
 
 
