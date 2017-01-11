585e17c8ea0aff6f422700718f683099b200c943
Ray Yuen <rayuen@gmail.com>
2017-01-10 16:07:26 -0800

    updated the html + php for the resume page so that it reads from an array

--- inc/resume.php
+++ inc/resume.php
@@ -1,37 +1,24 @@
 <?
 //last updated January 10, 2017
 
-$bio['about_me'] = "
-Graphic designer with extensive print and production experience. Background in illustration. Passionate about typography and game design. Learning more about programming languages.
-";
 
-$bio = array(
+$bio_array = array(
   "name" => "Raymond Yuen",
   "title" => "Graphic Designer",
   "phone" => "347 439 5576",
   "email" => "rayuen@gmail.com",
   "education" => "Rochester Institute of Technology<br>September 1998 - May 2002<br>BFA. Graphic Design",
 );
+$bio_array['about_me'] = "
+Graphic designer with extensive print and production experience. Background in illustration. Passionate about typography and game design. Learning more about programming languages.
+";
 
-// class Work {
-//   public $company;
-//   public $title;
-//   public $location;
-//   public $time_start;
-//   public $time_end;
-//   public $duties=array();
-// }
-
-// $nextlesson = new Work;
-// $nextlesson->title = "Head of Design";
-
-
-$work_history = array(
+$work_history_array = array(
 
   "nextlesson" => array(
     "job_title" => "Head of Design",
     "company" => "NextLesson",
-    "url" => "nextlesson.org",
+    "url" => "http://www.nextlesson.org",
     "description" => "Food truck Portland swag Pitchfork mustache semiotics, gluten-free raw denim ethical deep v hoodie YOLO Bushwick hella. Carles 3 wolf moon cred freegan slow-carb, banh mi direct trade High Life Helvetica street art. Narwhal 8-bit Portland, forage iPhone Pitchfork plaid blog Brooklyn cliche wayfarers pickled squid leggings. Mumblecore gluten-free Intelligentsia, Truffaut yr fingerstache wolf Blue Bottle Neutra Tumblr. Williamsburg 90's viral DIY literally. Craft beer kale chips iPhone quinoa whatever small batch. Meh Neutra scenester plaid, hella Tumblr 8-bit pug Helvetica pour-over sustainable Marfa.",
     "location" => "San Francisco",
     "start" => "9/2014",
@@ -44,20 +31,131 @@ $work_history = array(
     ),
   ),
 
-
-  "pokemon" => array(
-    "job_title" => "Head of Design",
-    "company" => "Pokemon",
-    "url" => "nextlesson.org",
+  "suspended-belief" => array(
+    "job_title" => "Co-founder and Creative Director",
+    "company" => "Suspended Belief Studios",
+    "url" => "",
     "description" => "Food truck Portland swag Pitchfork mustache semiotics, gluten-free raw denim ethical deep v hoodie YOLO Bushwick hella. Carles 3 wolf moon cred freegan slow-carb, banh mi direct trade High Life Helvetica street art. Narwhal 8-bit Portland, forage iPhone Pitchfork plaid blog Brooklyn cliche wayfarers pickled squid leggings. Mumblecore gluten-free Intelligentsia, Truffaut yr fingerstache wolf Blue Bottle Neutra Tumblr. Williamsburg 90's viral DIY literally. Craft beer kale chips iPhone quinoa whatever small batch. Meh Neutra scenester plaid, hella Tumblr 8-bit pug Helvetica pour-over sustainable Marfa.",
     "location" => "San Francisco",
-    "start" => "9/2014",
+    "start" => "2/2012",
+    "end" => "6/2014",
+    "duties" => array(
+      "Concept and design of 4 original games. Included level design, game flow, and scripting in-game events.",
+      "Created art assets for games. Included: original characters, backgrounds, objects and user interface.",
+      "Storyboarded level and animation sequences.",
+      "Worked with developers to iterate software.",
+      "Developed websites using HTML, CSS / Sass, PHP, jQuery. (see <a href='abriendo-puertas'><span class='nowrap'>ap-od.org</span></a>)",
+    ),
+  ),
+
+  "freelance" => array(
+    "job_title" => "Freelance Designer/Illustrator",
+    "company" => "Suspended Belief Studios",
+    "url" => "",
+    "description" => "Food truck Portland swag Pitchfork mustache semiotics, gluten-free raw denim ethical deep v hoodie YOLO Bushwick hella. Carles 3 wolf moon cred freegan slow-carb, banh mi direct trade High Life Helvetica street art. Narwhal 8-bit Portland, forage iPhone Pitchfork plaid blog Brooklyn cliche wayfarers pickled squid leggings. Mumblecore gluten-free Intelligentsia, Truffaut yr fingerstache wolf Blue Bottle Neutra Tumblr. Williamsburg 90's viral DIY literally. Craft beer kale chips iPhone quinoa whatever small batch. Meh Neutra scenester plaid, hella Tumblr 8-bit pug Helvetica pour-over sustainable Marfa.",
+    "location" => "San Francisco",
+    "start" => "4/2012",
     "end" => "current",
     "duties" => array(
-      "spelling",
-      "design",
-      "ui ux",
-      "illustrations",
+          "brochure design",
+          "infographics",
+          "presentation slides",
+          "website development",
+          "spot illustrations",
+          "tee shirt illustrations",
+          "mascot character designs",
+          "digital ad repurposing",
+          "map creation",
+    ),
+  ),
+
+  "freelance" => array(
+    "job_title" => "Graphic Designer",
+    "company" => "Freelance",
+    "url" => "",
+    "description" => "Food truck Portland swag Pitchfork mustache semiotics, gluten-free raw denim ethical deep v hoodie YOLO Bushwick hella. Carles 3 wolf moon cred freegan slow-carb, banh mi direct trade High Life Helvetica street art. Narwhal 8-bit Portland, forage iPhone Pitchfork plaid blog Brooklyn cliche wayfarers pickled squid leggings. Mumblecore gluten-free Intelligentsia, Truffaut yr fingerstache wolf Blue Bottle Neutra Tumblr. Williamsburg 90's viral DIY literally. Craft beer kale chips iPhone quinoa whatever small batch. Meh Neutra scenester plaid, hella Tumblr 8-bit pug Helvetica pour-over sustainable Marfa.",
+    "location" => "New York City",
+    "start" => "3/2010",
+    "end" => "4/2012",
+    "duties" => array(
+      "Design and layout of travel guides.",
+      "Implemented design of best-selling book.",
+      "Worked with editors to build content based on author’s content and suggestions.",
+      "Managed art assets and working files.",
+      "Proofed and fixed maps.",
+      "Refined the indexing process.",
+      "Fixed problems in prepress.",
+      "Prepared cover UV files for&nbsp;print.",
+      "Worked with printers to ensure quality.",
+    ),
+  ),
+
+
+  "pokemon" => array(
+    "job_title" => "In-house Graphic Designer",
+    "company" => "The Pok&eacute;mon Company",
+    "url" => "pokemon.com",
+    "description" => "Food truck Portland swag Pitchfork mustache semiotics, gluten-free raw denim ethical deep v hoodie YOLO Bushwick hella. Carles 3 wolf moon cred freegan slow-carb, banh mi direct trade High Life Helvetica street art. Narwhal 8-bit Portland, forage iPhone Pitchfork plaid blog Brooklyn cliche wayfarers pickled squid leggings. Mumblecore gluten-free Intelligentsia, Truffaut yr fingerstache wolf Blue Bottle Neutra Tumblr. Williamsburg 90's viral DIY literally. Craft beer kale chips iPhone quinoa whatever small batch. Meh Neutra scenester plaid, hella Tumblr 8-bit pug Helvetica pour-over sustainable Marfa.",
+    "location" => "New York City",
+    "start" => "9/2005",
+    "end" => "1/2010",
+    "duties" => array(
+      "Worked in a small team of three.",
+      "Serviced internal departments with creative needs.",
+      "Worked with marketing to strategize brand and product&nbsp;communication.",
+      "Assisted publishing and editorial teams with strategy guides. Took creation of smaller game guides in-house.",
+      "Created artwork and signage for major Pok&eacute;mon related events, like video game tournaments and launches, trade show booths, and new TV shows and movies.",
+      "Directed outside vendors to get artwork produced in timely and cost-effective manner.",
+      "Enforced brand standards internally.",
+      "Worked with licensing team to provide brand guidelines and art assets to&nbsp;licensees.",
+      "Created logos for new TV and movie series as well as promotion materials, like posters and packaging.",
+      "Developed promotional websites for new movies and side&nbsp;games.",
+      "Maintained and organized company art assets. Distributed as needed.",
+      "Developed new company stationary after office move. Included business cards, letterheads, envelopes.",
+      "Created various art assets for company events, including holiday partys. Things like invites, greeting cards signs, and vodka labels.",
+      "Worked with e-commerce department to retouch photos and localize Japanese products.",
+      "Created best selling tee shirt at Nintendo World Shop.",
+      "Implement an automated creation system for Pok&eacute;mon charts (left before I could finish this).",
+      "Kicked arse at company video game tournaments. Trading card game not so much.",
+    ),
+  ),
+
+  "equinox" => array(
+    "job_title" => "In-house Graphic Designer",
+    "company" => "Equinox Fitness Clubs",
+    "url" => "equinox.com",
+    "description" => "Food truck Portland swag Pitchfork mustache semiotics, gluten-free raw denim ethical deep v hoodie YOLO Bushwick hella. Carles 3 wolf moon cred freegan slow-carb, banh mi direct trade High Life Helvetica street art. Narwhal 8-bit Portland, forage iPhone Pitchfork plaid blog Brooklyn cliche wayfarers pickled squid leggings. Mumblecore gluten-free Intelligentsia, Truffaut yr fingerstache wolf Blue Bottle Neutra Tumblr. Williamsburg 90's viral DIY literally. Craft beer kale chips iPhone quinoa whatever small batch. Meh Neutra scenester plaid, hella Tumblr 8-bit pug Helvetica pour-over sustainable Marfa.",
+    "location" => "New York City",
+    "start" => "4/2004",
+    "end" => "9/2005",
+    "duties" => array(
+      "Oversaw 15+ gym’s collateral needs.",
+      "Print, cut, and packaged (for delivery) gym collateral. Included membership cards, free passes, promotional passes, posters.",
+      "Maintained company art assets.",
+      "Helped develop special marketing promotion creative.",
+      "Maintained OCE printer. Also trained designers how to use the printer effectively.",
+      "Ensured timely completion of projects.",
+      "Developed signage system and imagery for new Equinox&nbsp;gyms.",
+      "Interviewed and trained new designers and interns.",
+      "Pre-press for projects done by outside vendors.",
+      "Held down the fort when we were between studio&nbsp;managers.",
+      "Lost at daily plank competitions.",
+    ),
+  ),
+
+  "reporter" => array(
+    "job_title" => "Graphic Designer",
+    "company" => "Reporter Magazine",
+    "url" => "equinox.com",
+    "description" => "Food truck Portland swag Pitchfork mustache semiotics, gluten-free raw denim ethical deep v hoodie YOLO Bushwick hella. Carles 3 wolf moon cred freegan slow-carb, banh mi direct trade High Life Helvetica street art. Narwhal 8-bit Portland, forage iPhone Pitchfork plaid blog Brooklyn cliche wayfarers pickled squid leggings. Mumblecore gluten-free Intelligentsia, Truffaut yr fingerstache wolf Blue Bottle Neutra Tumblr. Williamsburg 90's viral DIY literally. Craft beer kale chips iPhone quinoa whatever small batch. Meh Neutra scenester plaid, hella Tumblr 8-bit pug Helvetica pour-over sustainable Marfa.",
+    "location" => "New York City",
+    "start" => "11/2001",
+    "end" => "5/2002",
+    "duties" => array(
+      "Worked with the photographers to lay out magazine&nbsp;content.",
+      "Magazine pre-press. Mistake correction and made sure all assets were handed to printers. ",
+      "Assisted creative director with a redesign of the&nbsp;magazine.",
+      "Made sure all assets needed for advertisements were delivered in a timely manner.",
     ),
   ),
 
