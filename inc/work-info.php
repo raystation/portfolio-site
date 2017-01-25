6f8d95f45108cfbaa81c38f038167015934f12ee
Ray Yuen <rayuen@gmail.com>
2017-01-24 23:39:44 -0800

    if you are viewing a project, it will not show up in the recent projects on the bottom so you can’t link to the same project you are on

--- inc/work-info.php
+++ inc/work-info.php
@@ -4,7 +4,7 @@ $works = array(
 	array( "name" => "NextLesson","path" => "nextlesson", "thumb" => "jpg" ),
 	array( "name" => "NextLesson Lesson Covers","path" => "nextlesson-covers", "thumb" => "jpg" ),
 	array( "name" => "NextLesson InterestID","path" => "nextlesson-interestid", "thumb" => "jpg" ),
-	array( "name" => "NextLesson Marketing","path" => "nextlesson-marketing", "thumb" => "jpg" ),
+	// array( "name" => "NextLesson Marketing","path" => "nextlesson-marketing", "thumb" => "jpg" ),
 	array( "name" => "NextLesson Glossary Images","path" => "nextlesson-glossary", "thumb" => "jpg" ),
 	array( "name" => "#Inktober2016","path" => "inktober-2016", "thumb" => "jpg" ),
 	// array( "name" => "NextLesson Interactive","path" => "nextlesson-interactive", "thumb" => "jpg" ),
