582125b02da35b758a8502f626b577253cbf7fbc
Ray Yuen <rayuen@gmail.com>
2017-03-15 20:08:49 -0700

    took out the ringtones

--- inc/footer.php
+++ inc/footer.php
@@ -48,19 +48,19 @@ if ( !isset($id)) {
 				<ul>
 					<li><a href="./<? echo $id_html;?>">home</a>
 					<li><a href="work<? echo $id_html;?>">work</a>
-					<li><a href="about<? echo $id_html;?>">about</a>
 				</ul>
 			</div>
 
 			<div class="four columns add-bottom">
 				<ul>
+					<li><a href="about<? echo $id_html;?>">about</a>
 					<li><a href="mailto:rayuen@gmail.com?body=<? echo $email_text;?>&subject=<? echo $email_subject;?>">email</a>
 				</ul>
 			</div>
 
 			<div class="four columns">
 				<ul>
-					<li><a href="ringtones.php">ringtones</a>
+					<!--<li><a href="ringtones.php">ringtones</a>-->
 					<!-- <li><a href="jobs">jobs</a> -->
 				</ul>
 			</div>
